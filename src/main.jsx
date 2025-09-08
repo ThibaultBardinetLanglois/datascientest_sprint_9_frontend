0d1edb3f028f045aa2d01af69e61b1e177b78d28
Thibault Bardinet-Langlois <langlois.bardinet.thibault@gmail.com>
2025-09-08 20:40:30 +0200

    change case syntax for Sentry

--- src/main.jsx
+++ src/main.jsx
@@ -7,37 +7,36 @@ import * as Sentry from "@sentry/react";
 
 // Test appel backend
 const apiClient = axios.create({baseURL: `${import.meta.env.VITE_API_URL}/api`});
-
-apiClient.get("health/")
+apiClient.get("/health/")
   .then(res => console.log("Réponse API:", res.data))
   .catch(err => console.error("Erreur API:", err));
 
 
 // On configure Sentry UNIQUEMENT en production
-    // On utilise la variable `import.meta.env.PROD`, fournie automatiquement par Vite
-    if (import.meta.env.PROD) {
-      Sentry.init({
-        dsn: import.meta.env.VITE_SENTRY_DSN, // On la charge depuis les variables d'env
+// On utilise la variable `import.meta.env.PROD`, fournie automatiquement par Vite
+if (import.meta.env.PROD) {
+  Sentry.init({
+    dsn: import.meta.env.VITE_SENTRY_DSN, // On la charge depuis les variables d'env
 
-        integrations: [
-          // Cette intégration active le monitoring de performance.
-          // Elle permet de voir, par exemple, combien de temps prennent les appels API.
-          Sentry.browserTracing(), 
+    integrations: [
+      // Cette intégration active le monitoring de performance.
+      // Elle permet de voir, par exemple, combien de temps prennent les appels API.
+      Sentry.browserTracingIntegration(), 
 
-          // Cette intégration enregistre les sessions utilisateur où une erreur se produit.
-          // C'est comme avoir une "vidéo" de ce que l'utilisateur a fait avant le bug.
-          Sentry.replay()
-        ],
+      // Cette intégration enregistre les sessions utilisateur où une erreur se produit.
+      // C'est comme avoir une "vidéo" de ce que l'utilisateur a fait avant le bug.
+      Sentry.replayIntegration()
+    ],
 
-        // On configure le taux d'échantillonnage pour la performance.
-        // 1.0 signifie qu'on capture 100% des transactions pour analyse.
-        traces_sample_rate: 1.0,
+    // On configure le taux d'échantillonnage pour la performance.
+    // 1.0 signifie qu'on capture 100% des transactions pour analyse.
+    tracesSampleRate: 1.0,
 
-        // On configure le taux d'échantillonnage pour les "replays" de session.
-        replays_session_sample_rate: 0.1,  // On enregistre 10% des sessions.
-        replays_on_error_sample_rate: 1.0,  // Mais on enregistre 100% des sessions où une erreur survient.
-      });
-    }
+    // On configure le taux d'échantillonnage pour les "replays" de session.
+    replaysSessionSampleRate: 0.1,  // On enregistre 10% des sessions.
+    replaysOnErrorSampleRate: 1.0,  // Mais on enregistre 100% des sessions où une erreur survient.
+  });
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
